5dab1baa70f371e7413d3c025755dd821c9bb86e
eduargiraldo <139786773+eduargiraldo@users.noreply.github.com>
2023-10-19 08:09:48 -0500

    suma.js

--- src/suma.js
+++ src/suma.js
@@ -1,16 +1,18 @@
-import { LitElement, html, render } from "lit-element";
-import styleScss from "./sumaStyle.js";
+import { LitElement, html } from "lit-element";
+
 
 export class Suma extends LitElement {
   static get styles() {
-    return [styleScss];
+    return;
   }
 
   sumar() {
     var numero1 = this.shadowRoot.getElementById("numero1").value;
     var numero2 = this.shadowRoot.getElementById("numero2").value;
-    if (numero1 !== "" && numero2 !== "") {
-      var suma = parseInt(numero1) + parseInt(numero2);
+    var numero3 = this.shadowRoot.getElementById("numero3").value;
+
+    if (numero1 !== "" && numero2 !== ""&& numero3 !=="") {
+      var suma = parseInt(numero1) + parseInt(numero2) * parseInt(numero3);
       this.shadowRoot.getElementById("resultado").value = suma;
     }
   }
@@ -23,6 +25,8 @@ export class Suma extends LitElement {
       <input type="text" id="numero1" @change="${this.sumar}"><br><br>
       <label for="numero2">Numero 2</label>
       <input type="text" id="numero2" @change="${this.sumar}"><br><br>
+      <label for="numero2">Numero 3*</label>
+      <input type="text" id="numero3" @change="${this.sumar}"><br><br>
       <label for="resultado">Resultado</label>
       <input type="text" id="resultado">
     `;
