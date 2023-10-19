1cd573f4941e8e5db5b68bc29242cd57709fd249
eduargiraldo <139786773+eduargiraldo@users.noreply.github.com>
2023-10-19 08:19:06 -0500

    multiplica.js

--- src/multiplica.js
+++ src/multiplica.js
@@ -9,8 +9,10 @@ export class Multiplica extends LitElement {
   multiplica() {
     var numero1 = this.shadowRoot.getElementById("numero1").value;
     var numero2 = this.shadowRoot.getElementById("numero2").value;
-    if (numero1 !== "" && numero2 !== "") {
-      var multiplica = parseInt(numero1) * parseInt(numero2);
+     var numero3 = this.shadowRoot.getElementById("numero3").value;
+
+    if (numero1 !== "" && numero2 !== ""&& numero3 !=="") {
+      var multiplica = parseInt(numero1) * parseInt(numero2)* parseInt(numero3)*1000;
       this.shadowRoot.getElementById("resultado").value = multiplica;
     }
   }
@@ -23,6 +25,8 @@ export class Multiplica extends LitElement {
       <input type="text" id="numero1" @change="${this.multiplica}"><br><br>
       <label for="numero2">Numero 2</label>
       <input type="text" id="numero2" @change="${this.multiplica}"><br><br>
+      <label for="numero2">Numero 3</label>
+      <input type="text" id="numero3" @change="${this.multiplica}"><br><br>
       <label for="resultado">Resultado</label>
       <input type="text" id="resultado">
     `;
