5aaf19c60e99e3bd8f971e8daf8eb559ef1417b2
eduargiraldo <139786773+eduargiraldo@users.noreply.github.com>
2023-10-19 11:28:18 -0500

    grados.js

--- src/grados.js
+++ src/grados.js
@@ -26,3 +26,42 @@ export class Grados extends LitElement {
 }
 
 customElements.define("my-grados", Grados);
+
+
+
+
+
+/////////////////////////////////////////////////////////////////RAIZ CUADRADA////////////////////////////////////////////////////////////////////////////
+import { LitElement, html } from "lit-element";
+
+export class Hipo extends LitElement {
+  static get style() {
+    return ;
+  }
+
+  Hipo() {
+    var numero1 = this.shadowRoot.getElementById("numero1").value;
+    var numero2 = this.shadowRoot.getElementById("numero2").value;
+
+    if (numero1 !== "" && numero2 !== "" ){
+      var Hipo = Math.sqrt(parseInt(numero1)*2 + parseInt(numero2)*2);
+      this.shadowRoot.getElementById("resultado").value = Hipo;
+    }
+  }
+ 
+  render() {
+    return html`
+
+      <h2>Descubrir la hipotenusa</h2>
+
+      <label for="numero1">Lado 1</label>
+      <input type="text" id="numero1" @change="${this.Hipo}"><br><br>
+      <label for="numero2">Lado 2</label>
+      <input type="text" id="numero2" @change="${this.Hipo}"><br><br>
+      <label for="resultado">Hipotenusa</label>
+      <input type="text" id="resultado">
+    `;
+  }
+}
+
+customElements.define("my-hipo", Hipo);
